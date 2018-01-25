d3c304b7c95b2f063d308ef9a33b58214aa76498
Susan <suzutoyuki@gmail.com>
2018-01-25 06:22:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var recipes = {breakfast: 'oatmeal'};
 
 function updateObjectWithKeyAndValue(object, key, value){
-
-object[key] = value;
 object.assign({},updateObjectWithKeyAndValue,{key:value});
+object[key] = value;
    return object ;
 
 }
